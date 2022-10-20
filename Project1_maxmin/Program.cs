fdbc0fade03a39a3965fdb2f019efaeba5cfa526
Svetlana <tsvetcova.s@mail.ru>
2022-10-20 19:58:58 +0300

    add Project 1 max min

---
+++ Project1_maxmin/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, определяющая большее и меньшее из двух чисел
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if(a > b)
+Console.WriteLine("Первое число больше второго");
+else
+Console.WriteLine("Второе число больше первого");
