cf128c12009ce4af13661179b2e5f8f567369d2a
Svetlana <tsvetcova.s@mail.ru>
2022-10-20 23:57:09 +0300

    add Project 4 even1-N

---
+++ Project4_even1-N/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, показывающая все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int I = 1;
+while(I <= N)
+{
+    if(I % 2 == 0)
+    {
+        Console.Write(I + " ");
+    }
+    I++;
+}
